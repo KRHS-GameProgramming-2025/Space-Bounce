034ffaaef10c1df403f39b1cc159e3a57513ef46
Aidan Ferreira <aidan.ferreira@kearsarge.org>
2025-01-14 07:48:53 -0500

    Copied items from balldemo

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
+import pygame, sys, math, random
+from Player import *
 
+
+clock = pygame.time.Clock();
+size = [900, 700]
+screen = pygame.display.set_mode(size)
--- Player.py
+++ Player.py
@@ -1,1 +1,55 @@
+import pygame, sys, math
+from Main import *
 
+class PlayerBall(Ball):
+     def __init__(self, maxSpeed=4, startPos=[0,0]):
+        Ball.__init__(self, [0,0], startPos)
+        self.image = [pygame.image.load("Images/Player/Ship.png")]
+        self.rect = self.image.get_rect()
+        
+        self.maxSpeed = maxSpeed
+        self.kind = "player"
+
+     def goKey(self, direction):
+        if direction == "left":
+            self.speedx = -self.maxSpeed
+        elif direction == "right":
+            self.speedx = self.maxSpeed
+        elif direction == "up":
+            self.speedy = -self.maxSpeed
+            self.images = self.imagesUp
+        elif direction == "down":
+            self.speedy = self.maxSpeed
+        elif direction == "sleft":
+            self.speedx = 0
+        elif direction == "sright":
+            self.speedx = 0
+        elif direction == "sup":
+            self.speedy = 0
+        elif direction == "sdown":
+            self.speedy = 0 
+            
+               
+     def wallCollide(self, size):
+        width = size[0]
+        height = size[1]
+        
+        if not self.didBounceY:
+            if self.rect.bottom > height:
+                self.didBounceY = True
+                
+            if self.rect.top < 0:
+                
+                
+        if not self.didBounceY:
+            if self.rect.right > width:
+                
+                
+            if self.rect.left < 0:
+                
+                
+            
+     def ballCollide(self, other):
+        if self != other:
+            print("e")
+           
